5e6e512a380cdd6b19b9be4a0adf36191abba439
Luis Alvarado Murillo <104881382+luismuri11@users.noreply.github.com>
2024-08-20 21:18:41 -0500

    Add files via upload

---
+++ Captioning_demo_using_gradio.py
@@ -0,0 +1,34 @@
+import gradio as gr
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+
+# Instantiate the model and the processor
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+#Create the function
+def generate_caption(image):
+    # Now directly using the PIL Image object
+    inputs = processor(images=image, return_tensors ='pt')
+    outputs = model.generate(**inputs, max_new_tokens=100)
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
+    return caption
+
+def caption_image(image):
+    """
+    Takes a PIL Image input and returns a caption.
+    """
+    try:
+        caption = generate_caption(image)
+        return caption
+    except Exception as e:
+        return f"An error ocurred: {str(e)}"
+
+interface = gr.Interface(
+    fn = caption_image,
+    inputs = gr.Image(type="pil"),
+    outputs = "text",
+    title = "Image Captioning with BLIP",
+    description = "Upload an image to generate a caption."
+)
+interface.launch()
